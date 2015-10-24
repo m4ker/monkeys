29819eb1ca9fba4dd9b4e1ffb894d57bb1e5d62b
王默 <maker@maker-air.local>
2015-10-24 14:42:03 +0800

    模板文件创建

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>标配 is comming ... </title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">网站建设中……</div>
+            </div>
+        </div>
+    </body>
+</html>
