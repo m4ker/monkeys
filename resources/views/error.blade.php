a83bcf276e380aaf3e5e64deac9412a88c5b2bd6
王默 <maker@maker-air.local>
2015-10-24 16:47:24 +0800

    优化错误页面

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -1,5 +1,14 @@
-
-<?php echo $msg;?>
-<hr />
-<a href="javascript:history.go(-1);">返回</a>
+<html>
+<head>
+    <meta charcode="UTF-8" />
+    <title><?php echo $msg;?></title>
+    <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+</head>
+<body style="text-align:center;">
+    <h1><?php echo $msg;?></h1>
+    <hr />
+    <a href="javascript:history.go(-1);">返回</a>
+</body>
+</html>
 
