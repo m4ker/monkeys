5d761f37dc4965daf11b6155e4a2e405477560f3
m4ker <makerwang@gmail.com>
2015-10-25 03:54:56 +0800

    干掉缓存

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -20,7 +20,7 @@
     <link href="/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="/css/agency.css" rel="stylesheet">
+    <link href="/css/agency.css?<?php echo time();?>" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -15,7 +15,7 @@
     <link href="/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="/css/agency.css" rel="stylesheet">
+    <link href="/css/agency.css?<?php echo time();?>" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
