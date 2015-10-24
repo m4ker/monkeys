3f993a14f4a9d1fed9b26dfb7cec2377f2e50b2a
王默 <maker@maker-air.local>
2015-10-24 14:08:35 +0800

    init controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,16 +11,24 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// 首页
+Route::get('/', function(){
+	return view('index');
 });
+// 创建活动
+Route::get('/channel/create', 'Channel@create');
+// 创建活动后端
+Route::post('/channel/create', 'Channel@create');
+// 创建成功
+Route::get('/channel/{cid}/success', 'Channel@success');
 
-/*
-Route::get('test', function () {
-    return 'test 2';
-});
-*/
+// 频道用户列表
+Route::get('/channel/{cid}', 'Channel@list');
+// 登记
+Route::get('/channel/{cid}/register', 'User@register');
+// 登记后端
+Route::post('/channel/{cid}/register', 'User@register');
+// 用户智能匹配
+Route::get('/channel/{cid}/suggest/{uid}', 'User@suggest');
 
-Route::get('test', 'test@index');
 
-//Route::get('test/{id}', 'test@index');
